cde7a8d694cb0c58f44fba4b34c741951265609c
Riccardo Renzulli <renzulli.riccardo@gmail.com>
2017-05-25 11:36:51 +0200

    Added map3 english version.

--- labirinto/Maps/map3-20x20.pl
+++ labirinto/Maps/map3-20x20.pl
@@ -7,37 +7,37 @@
 % giovanni.bonetta@edu.unito.it
 
 num_col(20).
-num_righe(20).
+num_rows(20).
 
-occupata(pos(7,15)).
-occupata(pos(8,15)).
-occupata(pos(9,15)).
-occupata(pos(10,15)).
-occupata(pos(11,15)).
-occupata(pos(12,15)).
-occupata(pos(13,15)).
+occupied(pos(7,15)).
+occupied(pos(8,15)).
+occupied(pos(9,15)).
+occupied(pos(10,15)).
+occupied(pos(11,15)).
+occupied(pos(12,15)).
+occupied(pos(13,15)).
 
-occupata(pos(13,6)).
-occupata(pos(13,7)).
-occupata(pos(13,8)).
-occupata(pos(13,9)).
-occupata(pos(13,10)).
-occupata(pos(13,11)).
-occupata(pos(13,12)).
-occupata(pos(13,13)).
-occupata(pos(13,14)).
+occupied(pos(13,6)).
+occupied(pos(13,7)).
+occupied(pos(13,8)).
+occupied(pos(13,9)).
+occupied(pos(13,10)).
+occupied(pos(13,11)).
+occupied(pos(13,12)).
+occupied(pos(13,13)).
+occupied(pos(13,14)).
 
-occupata(pos(15,1)).
-occupata(pos(15,2)).
-occupata(pos(15,3)).
-occupata(pos(15,4)).
-occupata(pos(15,5)).
-occupata(pos(15,6)).
-occupata(pos(15,7)).
-occupata(pos(15,8)).
-occupata(pos(15,9)).
+occupied(pos(15,1)).
+occupied(pos(15,2)).
+occupied(pos(15,3)).
+occupied(pos(15,4)).
+occupied(pos(15,5)).
+occupied(pos(15,6)).
+occupied(pos(15,7)).
+occupied(pos(15,8)).
+occupied(pos(15,9)).
 
 
-iniziale(pos(10,10)).
+initial(pos(10,10)).
 
-finale(pos(20,20)).
+final(pos(20,20)).
