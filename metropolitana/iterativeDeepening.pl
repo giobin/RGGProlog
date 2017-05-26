cf63e3240799d87cabbd008807afb0b982752b42
the-silent-fox <sartor.gabriele23@gmail.com>
2017-05-26 11:46:20 +0200

    Added metropolitana/iterativeDeepening.pl.

---
+++ metropolitana/iterativeDeepening.pl
@@ -0,0 +1,35 @@
+% Authors: Riccardo Renzulli, Gabriele Sartor
+% Università degli Studi di Torino
+% Department of Computer Science
+% Date: September 2016
+% riccardo.renzulli@edu.unito.it
+% gabriele.sartor@edu.unito.it
+
+% consult('utility.pl'), consult('iterativedeeping.pl'), startItDeep.
+
+% Iterative deepening depth-first search
+startItDeep :-
+  retractall(currentMaxDepth(_)),
+	assertz(currentMaxDepth(0)),
+	iterativeDeepening(Moves),
+	write(Moves).
+
+iterativeDeepening(Moves) :-
+	initial(InitialState),
+	it_deep(InitialState,0,[InitialState],Moves).
+iterativeDeepening(Moves) :-
+	currentMaxDepth(MaxDepth),
+	NewMaxDepth is MaxDepth + 1,
+	retractall(currentMaxDepth(_)),
+	assertz(currentMaxDepth(NewMaxDepth)),
+	iterativeDeepening(Moves).
+
+it_deep(State,_,_,[]) :- final(State),!.
+it_deep(State,Depth,Visited,[Action|MovesSequence]) :-
+	NewDepth is Depth + 1,
+	currentMaxDepth(MaxDepth),
+	NewDepth =< MaxDepth,
+	applicable(Action,State),
+	transform(Action,State,NewState),
+	\+member(NewState,Visited),
+	it_deep(NewState,NewDepth,[NewState|Visited],MovesSequence).
