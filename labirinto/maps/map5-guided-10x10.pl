4d45fa23052f141ef424cce40e06a329404aa33c
Riccardo Renzulli <renzulli.riccardo@gmail.com>
2017-05-26 17:10:16 +0200

    Added guided map in labirinto.

---
+++ labirinto/maps/map5-guided-10x10.pl
@@ -0,0 +1,64 @@
+% Authors: Riccardo Renzulli, Gabriele Sartor, Giovanni Bonetta
+% Università degli Studi di Torino
+% Department of Computer Science
+% Date: May 2017
+% riccardo.renzulli@edu.unito.it
+% gabriele.sartor@edu.unito.it
+% giovanni.bonetta@edu.unito.it
+
+num_col(10).
+num_rows(10).
+
+initial(pos(1,1)).
+
+%occupied(pos(1,2)).
+occupied(pos(2,2)).
+occupied(pos(3,2)).
+occupied(pos(4,2)).
+occupied(pos(5,2)).
+occupied(pos(6,2)).
+occupied(pos(7,2)).
+occupied(pos(8,2)).
+occupied(pos(9,2)).
+
+occupied(pos(2,4)).
+occupied(pos(3,4)).
+occupied(pos(4,4)).
+occupied(pos(5,4)).
+occupied(pos(6,4)).
+occupied(pos(7,4)).
+occupied(pos(8,4)).
+occupied(pos(9,4)).
+occupied(pos(10,4)).
+
+occupied(pos(1,6)).
+occupied(pos(2,6)).
+occupied(pos(3,6)).
+occupied(pos(4,6)).
+occupied(pos(5,6)).
+occupied(pos(6,6)).
+occupied(pos(7,6)).
+occupied(pos(8,6)).
+occupied(pos(9,6)).
+
+occupied(pos(2,8)).
+occupied(pos(3,8)).
+occupied(pos(4,8)).
+occupied(pos(5,8)).
+occupied(pos(6,8)).
+occupied(pos(7,8)).
+occupied(pos(8,8)).
+occupied(pos(9,8)).
+occupied(pos(10,8)).
+
+occupied(pos(1,10)).
+occupied(pos(2,10)).
+occupied(pos(3,10)).
+occupied(pos(4,10)).
+occupied(pos(5,10)).
+occupied(pos(6,10)).
+occupied(pos(7,10)).
+occupied(pos(8,10)).
+occupied(pos(9,10)).
+
+final(pos(10,10)).
