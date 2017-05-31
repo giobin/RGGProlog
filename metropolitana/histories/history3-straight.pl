eba2a1826bb363134b82b2f8e4846a4aaeecb8b1
giobin <bonettagiovanni@gmail.com>
2017-05-31 18:30:40 +0200

    history2 and history3 added to metropolitana

---
+++ metropolitana/histories/history3-straight.pl
@@ -0,0 +1,11 @@
+% Authors: Riccardo Renzulli, Gabriele Sartor, Giovanni Bonetta
+% Università degli Studi di Torino
+% Department of Computer Science
+% Date: May 2017
+% riccardo.renzulli@edu.unito.it
+% gabriele.sartor@edu.unito.it
+% giovanni.bonetta@edu.unito.it
+
+initial([at('Notting Hill Gate'),ground]).
+
+final([at('Bank'),ground]).
